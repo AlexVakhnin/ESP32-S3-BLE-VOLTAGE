0984b3379c0c72dfa845374c7d091bf2b06f2488
AlexVakhnin <vakhninou@gmail.com>
2026-07-28 21:59:43 +0300

    update

---
+++ src/BLE.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+
+bool _BLEClientConnected = false;
+#define BatteryService BLEUUID((uint16_t)0x180F)
+
+BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        Serial.print("Event-Connect..");//Serial.println(remoteAddress);
+        _BLEClientConnected = true;
+    };
+    void onDisconnect(BLEServer* pServer) {
+        _BLEClientConnected = false;
+        Serial.println("Event-Disconnect..");
+        delay(300); // give the bluetooth stack the chance to get things ready
+        BLEDevice::startAdvertising();  // restart advertising
+    }
+};
+
+void ble_init(){
+    BLEDevice::init("UPS-PC-WIN11"); //init BLE stack..
+    // Create the BLE Server
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Service
+    BLEService *pBattery = pServer->createService(BatteryService);
+    pBattery->addCharacteristic(&BatteryLevelCharacteristic);
+    BatteryLevelDescriptor.setValue("Percentage 0 - 100");
+    BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
+    BatteryLevelCharacteristic.addDescriptor(new BLE2902());
+    pServer->getAdvertising()->addServiceUUID(BatteryService);
+    pBattery->start();
+    // Start advertising
+    pServer->getAdvertising()->start();
+}
+
+//change BLE service value
+void update_battery_level(uint8_t blevel){ 
+
+  BatteryLevelCharacteristic.setValue(&blevel, 1);
+  BatteryLevelCharacteristic.notify();
+}
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+//#include <BLEDevice.h>
+//#include <BLEServer.h>
+//#include <BLEUtils.h>
+//#include <BLE2902.h>
+extern void ble_init();
+extern void update_battery_level(uint8_t blevel);
+
+
+void setup() {
+
+    Serial.begin(115200);
+
+    delay(1000);
+    Serial.println();
+    Serial.println("----------------Start Info---------------");
+    Serial.printf("Total heap:\t%d \r\n", ESP.getHeapSize());
+    Serial.printf("Free heap:\t%d \r\n", ESP.getFreeHeap());
+    Serial.printf("Total PSRAM:\t%d \r\n", ESP.getPsramSize());
+    Serial.printf("Free PSRAM:\t%d \r\n", ESP.getFreePsram());
+    Serial.printf("Flash size:\t%d (bytes)\r\n", ESP.getFlashChipSize());
+    Serial.println("-----------------------------------------");
+
+    ble_init();
+
+    Serial.println("OK!-START..");
+
+}
+
+uint8_t battery_level = 0;
+
+void loop() {
+  update_battery_level(battery_level);  //change BLE service value
+  Serial.println(int(battery_level));
+
+  delay(5000);
+
+  battery_level++;
+  if (int(battery_level) == 100)
+    battery_level = 0;
+
+}
+
