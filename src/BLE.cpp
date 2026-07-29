decb2a18d357069d8295ebaa1af723e29804ff41
AlexVakhnin <vakhninou@gmail.com>
2026-07-29 22:39:30 +0300

    update

--- src/BLE.cpp
+++ src/BLE.cpp
@@ -4,17 +4,24 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+//Bluetooth 16-bit UUID Numbers (from revision 2022-05-19)
+//0x180F Battery
+//0x2A19 Battery Level
+//0x2901 Characteristic User Description
+//0x2902 Client Characteristic Configuration
+
 
 bool _BLEClientConnected = false;
-#define BatteryService BLEUUID((uint16_t)0x180F)
+#define BatteryServiceUUID BLEUUID((uint16_t)0x180F) //set Service UUID=>"Battery"
 
+//Create Characteristic with their UUID and Properties=(R,N)
 BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901)); //Create Descriptor
 
-
+//Callback functions
 class MyServerCallbacks : public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
-        Serial.print("Event-Connect..");//Serial.println(remoteAddress);
+        Serial.println("Event-Connect..");
         _BLEClientConnected = true;
     };
     void onDisconnect(BLEServer* pServer) {
@@ -31,15 +38,14 @@ void ble_init(){
     BLEServer *pServer = BLEDevice::createServer();
     pServer->setCallbacks(new MyServerCallbacks());
     // Create the BLE Service
-    BLEService *pBattery = pServer->createService(BatteryService);
+    BLEService *pBattery = pServer->createService(BatteryServiceUUID);
     pBattery->addCharacteristic(&BatteryLevelCharacteristic);
-    BatteryLevelDescriptor.setValue("Percentage 0 - 100");
-    BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
-    BatteryLevelCharacteristic.addDescriptor(new BLE2902());
-    pServer->getAdvertising()->addServiceUUID(BatteryService);
-    pBattery->start();
-    // Start advertising
-    pServer->getAdvertising()->start();
+    BatteryLevelDescriptor.setValue("Percentage 0 - 100");  //Descriptor init
+    BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);  //Descriptor add
+    BatteryLevelCharacteristic.addDescriptor(new BLE2902());  //for notifications control
+    pServer->getAdvertising()->addServiceUUID(BatteryServiceUUID);  //Advertising init
+    pBattery->start(); //Service start
+    pServer->getAdvertising()->start(); //Advertising start
 }
 
 //change BLE service value
